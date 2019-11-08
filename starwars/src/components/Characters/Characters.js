23932b23517b3576cb48be0014a7099521e446eb
John Afolabi <johnafolabi.da@gmail.com>
2019-11-08 11:58:52 +0100

    Style buttons

--- starwars/src/components/Characters/Characters.js
+++ starwars/src/components/Characters/Characters.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Container, Button } from "reactstrap";
+import { Container, Button, Col, Row } from "reactstrap";
 import CharacterCard from "./CharacterCard";
 
 function Characters() {
@@ -12,30 +12,38 @@ function Characters() {
       .get(`https://swapi.co/api/people/?format=json&page=${page}`)
       .then(response => {
         setPeople(response.data.results);
-        console.log(page);
-        
       })
       .catch(error => {});
   }, [page]);
 
   return (
     <Container>
-        <div className="characterslist">
-      {people.map(person => {
-        return (
-          <CharacterCard
-            key={person.name}
-            name={person.name}
-            gender={person.gender}
-            birth_year={person.birth_year}
-            height={person.height}
-            mass={person.mass}
-          />
-        );
-      })}
+      <div className="characterslist">
+        {people.map(person => {
+          return (
+            <CharacterCard
+              key={person.name}
+              name={person.name}
+              gender={person.gender}
+              birth_year={person.birth_year}
+              height={person.height}
+              mass={person.mass}
+            />
+          );
+        })}
       </div>
-      <Button color="primary" onClick={() => setPage(page - 1)}>Previous Page</Button>
-      <Button color="primary" onClick={() => setPage(page + 1)}>Next Page</Button>
+      <Row>
+        <Col xs="6">
+          <Button color="primary" onClick={() => setPage(page - 1)}>
+            Previous Page
+          </Button>
+        </Col>
+        <Col xs="6">
+          <Button color="primary" onClick={() => setPage(page + 1)}>
+            Next Page
+          </Button>
+          </Col>
+      </Row>
     </Container>
   );
 }
